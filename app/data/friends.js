de420d90c4be36940d38407f27196d9cd2f14599
peter bernard <peter.bernard3@icloud.com>
2020-03-14 02:52:31 -0400

    added some functionality

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var userData = [
+    {
+        "name": "Ahmed",
+        "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Joe",
+        "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+        "scores": [
+            3,
+            2,
+            2,
+            4,
+            1,
+            1,
+            5,
+            1,
+            3,
+            1
+        ]
+    },
+    {
+        "name": "Jane",
+        "photo": "https://avatars0.githubusercontent.com/u/1561825?v=3&s=460",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = userData;
